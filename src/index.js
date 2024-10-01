e82765e5d1b8ad8172d1cdf8b66479b1c308bd6d
stefano <30480337+snebo@users.noreply.github.com>
2024-10-01 23:24:17 +0100

    add firestore database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import {initializeApp} from 'firebase/app'
+import {getFirestore, collection, getDocs} from 'firebase/firestore'
+// import { snapshot } from 'node:test';
 
 const firebaseConfig = {
     apiKey: "AIzaSyATllaV9CO9I957w87Hz973CzhYoOhI9RE",
@@ -10,7 +12,26 @@ const firebaseConfig = {
     appId: "1:402872850268:web:ebb11e6f4c008a23b41399",
     measurementId: "G-PYCJ0F3385"
   };
+// initiilize the app
+initializeApp(firebaseConfig);
 
-  initializeApp(firebaseConfig);
+// initilize firesotre db
+const db = getFirestore()
 
+//collection refrence
+const colRef = collection(db, 'books')
+
+// basically loads the collexction and returns a promise
+getDocs(colRef)
+    .then((snapshot) =>{
+    // console.log(snapshot.docs) // docs represents all fo the data
+    let books = []
+    snapshot.docs.forEach((doc)=>{
+        books.push({...doc.data(), id: doc.id})
+    })
+    console.log(books)
+})
+.catch(err =>{
+    console.log(err.message)
+})
   
